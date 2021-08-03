b9158ee5c6dd3e7a717342dae7c3fd28e4ff3eb4
Andre Viana <code.andre3@gmail.com>
2021-08-03 16:32:53 -0300

    Finish containers-product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'webApp';
+
+  constructor() { }
+
+  ngOnInit(){
+
+  }
+
+  url: string = "../assets/img_1.jpg"
+  url_1: string = "../assets/img_2.jpg"
+  url_2: string = "../assets/img_3.jpg"
+  url_3: string = "../assets/img_4.jpg"
+
+  changeImage(event: any){
+    this.url = event.target.src;
+  }
+
+  changeImage1(event: any){
+    this.url_1 = event.target.src;
+  }
+  changeImage2(event: any){
+    this.url_2 = event.target.src;
+  }
+  changeImg(event: any){
+    this.url_3 = event.target.src;
+  }
 }
